31534d112e0a23adee8e50fe44c9a6a42f99a200
devforfu <developer.z@outlook.com>
2019-08-27 15:59:55 +0200

    Better logging

--- pool/pool.go
+++ pool/pool.go
@@ -1,10 +1,12 @@
 package pool
 
 import (
+	"fmt"
 	"log"
 )
 
 type Pool struct {
+	ID int
 	NumWorkers int
 	Jobs chan Job
 	Results chan Result
@@ -12,13 +14,22 @@ type Pool struct {
 	Done chan bool
 }
 
+var poolId = 0
+
 func NewPool(n int) *Pool {
-	return &Pool{
+	p := &Pool{
+		ID:poolId,
 		NumWorkers: n,
 		Jobs: make(chan Job),
 		Results: make(chan Result),
 		Stop: make(chan bool),
 		Done: make(chan bool)}
+	poolId += 1
+	return p
+}
+
+func (p *Pool) Log(format string, args ...interface{}) {
+	log.Printf("[pool:%d] %s", p.ID, fmt.Sprintf(format, args...))
 }
 
 func (p *Pool) Submit(jobs []Job, function ExecFunc) {
@@ -40,7 +51,7 @@ func (p *Pool) Chain(prev <-chan Result, function ExecFunc) {
 	go func() {
 		for result := range prev {
 			if err := result.Error; err != nil {
-				log.Printf("previous result has error: %s", err.Error())
+				p.Log("previous result has error: %s", err.Error())
 			} else {
 				p.Jobs <- Job{Payload:result.Payload}
 			}
@@ -58,7 +69,7 @@ func (p *Pool) Wait() []Result {
 		select {
 		case result, ok := <- p.Results:
 			if !ok {
-				log.Printf("execution ended")
+				p.Log("execution ended")
 				return results
 			} else {
 				results = append(results, result)
@@ -66,6 +77,7 @@ func (p *Pool) Wait() []Result {
 		case <- p.Done:
 			keepWorking -= 1
 			if keepWorking == 0 {
+				p.Log("no more results expected, closing channel")
 				close(p.Results)
 			}
 		}
@@ -78,7 +90,7 @@ func (p *Pool) prepareWorkers(f ExecFunc) []*Worker {
 		worker := Worker{
 			ID:i, Function:f,
 			Done:p.Done, Stop:p.Stop, Pool:p}
-		log.Printf("adding worker with ID=%d", worker.ID)
+		p.Log("adding worker with ID=%d", worker.ID)
 		workers = append(workers, &worker)
 	}
 	return workers
@@ -86,7 +98,7 @@ func (p *Pool) prepareWorkers(f ExecFunc) []*Worker {
 
 func (p *Pool) spawnWorkers(workers []*Worker) {
 	for _, worker := range workers {
-		log.Printf("submitting worker with ID=%d", worker.ID)
+		p.Log("submitting worker with ID=%d", worker.ID)
 		go worker.Submit(p.Jobs, p.Results)
 	}
 }
